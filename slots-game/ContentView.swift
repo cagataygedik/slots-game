6f0b5b6e12e39fad986f2d13f2f028bc51944a9a
cagatay <cagatay.gedik@icloud.com>
2022-09-07 10:58:00 +0300

    Initial Commit

---
+++ slots-game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slots-game
+//
+//  Created by Celil Çağatay Gedik on 7.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slots-game/slots_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  slots_gameApp.swift
+//  slots-game
+//
+//  Created by Celil Çağatay Gedik on 7.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct slots_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
